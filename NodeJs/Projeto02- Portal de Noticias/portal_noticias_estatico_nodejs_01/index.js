022025605785143242808821b560f05bf3fc0b44
qguit <gpereira@aluno.ufrb.edu.br>
2024-05-17 19:51:00 -0300

    add Packges init

---
+++ NodeJs/Projeto02- Portal de Noticias/portal_noticias_estatico_nodejs_01/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+var bodyParser = require('body-parser')
+
+const path = require('path');
+
+const app = express();
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/pages'));
+
+
+app.get('/',(req,res)=>{
+    
+    if(req.query.busca == null){
+        res.render('home',{});
+    }else{
+        res.send('Você buscou: '+req.query.busca);
+    }
+
+  
+});
+
+
+app.get('/:slug',(req,res)=>{
+    res.send(req.params.slug);
+})
+
+
+
+app.listen(5000,()=>{
+    console.log('server rodando!');
+})
